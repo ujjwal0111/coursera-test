fcd012732ad145bd67c96d2f0f93925f9bd6f42d
UJJWAL SINGH <ujjwal.2019@vitbhopal.ac.in>
2020-07-12 04:56:56 +0530

    js

---
+++ module4-solution/js/script.js
@@ -0,0 +1,16 @@
+(function () {
+	var names=["yaadov","John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+	for(var name in names) {
+		var firstLetter= names[name].charAt(0).toLowerCase();
+		if(firstLetter=='j'){
+			byeSpeaker.speak(names[name]);
+		}
+		else if(firstLetter=='J'){
+			byeSpeaker.speak(names[name]);
+		}
+	else {
+		helloSpeaker.speak(names[name]);
+	}	
+
+	}
+}) ();
